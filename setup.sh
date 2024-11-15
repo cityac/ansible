21a973689357f6b3ea227f0442941d71df3bafee
Stanislav Aituhanov <s.aituhanov@mmdsmart.com>
2024-11-15 11:03:55 +0200

    Git projects & setup linux desktop

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt-get update
 sudo apt-get install ansible
-touch .vault
+sudo apt install ubuntu-desktop xrdp
 
 sudo usermod -aG sudo $LOGNAME
 echo "$LOGNAME ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
